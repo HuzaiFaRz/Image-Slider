e4a168220f30d9f98b9244c8738f2dbff612aa24
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-07-25 06:08:28 -0700

    Some

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const sliderImages = document.querySelectorAll(".slider-img");
 const sliderNextButton = document.querySelector(".slider-next-btn");
 const sliderPrevButton = document.querySelector(".slider-prev-btn");
-let sliderImagesIndex = 1;
+let sliderImagesIndex = 0;
+
 const nextSlideImg = () => {
   if (sliderImagesIndex < sliderImages.length) {
     sliderImages.forEach((e) => {
@@ -11,7 +12,31 @@ const nextSlideImg = () => {
         ease: Expo.easeInOut,
       });
     });
-    sliderImagesIndex++;
+  } else {
+    sliderImagesIndex = 0;
+    sliderImages.forEach((e) => {
+      gsap.to(e, {
+        x: 0,
+        duration: 0.3,
+        ease: Expo.easeInOut,
+      });
+    });
+  }
+};
+sliderNextButton.addEventListener("click", () => {
+  sliderImagesIndex++;
+  nextSlideImg();
+});
+
+const prevSlideImg = () => {
+  if (sliderImagesIndex < sliderImages.length) {
+    sliderImages.forEach((e) => {
+      gsap.to(e, {
+        x: `${sliderImagesIndex * 100}%`,
+        duration: 0.3,
+        ease: Expo.easeInOut,
+      });
+    });
   } else {
     sliderImagesIndex = 0;
     sliderImages.forEach((e) => {
@@ -23,6 +48,7 @@ const nextSlideImg = () => {
     });
   }
 };
-sliderNextButton.addEventListener("click", () => {
-  nextSlideImg();
+sliderPrevButton.addEventListener("click", () => {
+  sliderImagesIndex--;
+  prevSlideImg();
 });
