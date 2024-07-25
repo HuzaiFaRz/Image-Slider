1c72237f97069c165ff234432c7bd4dbbe265a4f
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-07-25 04:46:47 -0700

    Some

--- app.js
+++ app.js
@@ -1,76 +1,33 @@
+const sliderImages = document.querySelectorAll(".slider-img");
 const sliderNextButton = document.querySelector(".slider-next-btn");
 const sliderPrevButton = document.querySelector(".slider-prev-btn");
-const sliderImages = document.querySelectorAll(".slider-img");
 let sliderImagesIndex = 1;
-let sliderImagesAnimation = false;
 
 const nextSlideImg = () => {
-  if (sliderImagesIndex < sliderImages.length - 1) {
+  if (sliderImagesIndex <= sliderImages.length) {
     gsap.to(sliderImages, {
-      // x: `${sliderImagesIndex * -100}%`,
-      x: "-=100%",
-      duration: 0.5,
+      x: `${sliderImagesIndex * -100}%`,
+      duration: 0.3,
       ease: Expo.easeInOut,
       onComplete: function () {
         gsap.set(this._targets[0], {
-          x: `100%`,
+          x: `${this._targets[0] * 100}%`,
         });
       },
     });
+
     sliderImagesIndex++;
-  } else if (sliderImagesIndex === sliderImages.length - 1) {
+  } else {
     sliderImagesIndex = 1;
+    // sliderImages.forEach((e) => {
+    //   gsap.to(e, {
+    //     x: 0,
+    //     duration: 0.3,
+    //     ease: Expo.easeInOut,
+    //   });
+    // });
   }
 };
 sliderNextButton.addEventListener("click", () => {
   nextSlideImg();
 });
-
-// sliderPrevButton.addEventListener("click", () => {
-//   prevSlideImg();
-// });
-// const prevSlideImg = () => {
-
-//     if (sliderImagesIndex < sliderImages.length) {
-//       gsap.to(sliderImages, {
-//         x: "0%",
-//         duration: 0.5,
-//         ease: Power4.easeInOut,
-//       });
-//       sliderImagesIndex--;
-//     } else if (sliderImagesIndex === sliderImages.length) {
-//       console.log("complted");
-//       sliderImagesIndex = 0;
-//       gsap.to(sliderImages, {
-//         x: "100%",
-//       });
-//     }
-//   });
-// };
-
-// sliderNextButton.addEventListener("click", () => {
-//   if (sliderImagesIndex >= 0) {
-//     gsap.to(elem, {
-//       x: "-=100%",
-//     });
-//     sliderImagesIndex++;
-//     gsap.to(elem, {
-//       x: "-=100%",
-//     });
-//     sliderImagesIndex++;
-//   }
-
-//   if (sliderImagesIndex === sliderImages.length) {
-//     console.log("complted");
-//     sliderImagesIndex = 0;
-//   }
-// });
-
-// console.log(sliderImagesIndex);
-
-// sliderNextButton.addEventListener("click", () => {
-//   sliderImagesIndex++;
-//   gsap.to(elem, {
-//     x: "-100%",
-//   });
-// });
