53cd8326878e955480852bd18378390b0d4832f4
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-08-07 05:40:50 -0700

    Commit

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const sliderInnner = document.querySelector(".slider-inner");
 const sliderNextButton = document.querySelector(".slider-next-btn");
 const sliderPrevButton = document.querySelector(".slider-prev-btn");
-const sliderImages = document.querySelectorAll(".slider-img");
 let imageIndex = 0;
+const imagesArray = [
+  "1-Img.jpg",
+  "2-Img.jpg",
+  "3-Img.jpg",
+  "4-Img.jpg",
+  "5-Img.jpg",
+];
+for (let i = 0; i < imagesArray.length; i++) {
+  let slideDiv = document.createElement("div");
+  let imagediv = document.createElement("img");
+  slideDiv.classList.add("slider-item");
+  imagediv.classList.add("slider-img");
+  slideDiv.appendChild(imagediv);
+  sliderInnner.appendChild(slideDiv);
+  imagediv.setAttribute("src", `${imagesArray[i]}`);
+}
 
-const nextSlideImg = () => {
+const sliderImages = document.querySelectorAll(".slider-img");
+
+sliderNextButton.addEventListener("click", () => {
   if (imageIndex >= sliderImages.length - 1) {
     imageIndex = 0;
   } else {
@@ -13,10 +30,9 @@ const nextSlideImg = () => {
   Array.from(sliderImages).forEach((sliderImagesElem) => {
     sliderImagesElem.style.transform = `translateX(-${imageIndex * 100}%)`;
   });
-};
-sliderNextButton.addEventListener("click", nextSlideImg);
+});
 
-const prevSlideImg = () => {
+sliderPrevButton.addEventListener("click", () => {
   if (imageIndex <= 0) {
     console.log(true);
     imageIndex = sliderImages.length - 1;
@@ -27,7 +43,4 @@ const prevSlideImg = () => {
   Array.from(sliderImages).forEach((sliderImagesElem) => {
     sliderImagesElem.style.transform = `translateX(-${imageIndex * 100}%)`;
   });
-};
-sliderPrevButton.addEventListener("click", prevSlideImg);
-
-console.log(2 - 3);
+});
